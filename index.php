e0ce2fdb3d13e59f966ac1cf5f8d81dbf89701c2
Elliot Parker <elliotccp@gmail.com>
2019-06-02 14:31:27 +1000

    Update variables

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+<?php $current = $_GET['l']; ?>
+
 <form action="/" method="GET">
     <select onchange="this.form.submit()" name="l">
         <option value="">Select Checklist</option>
     <?php foreach(glob("lists/*.csv") as $filename) {
-        $checklist = substr($filename,strpos($filename,"/") + 1, -4); ?>
-        <option value="<?php echo $checklist; ?>" <?php if($_GET['l'] == $checklist) {echo "selected";} ?>><?php echo $checklist; ?></option>
+        $item = substr($filename,strpos($filename,"/") + 1, -4); ?>
+        <option value="<?php echo $item; ?>" <?php if($current == $item) {echo "selected";} ?>><?php echo $item; ?></option>
     <?php } ?>
     </select>
     
 </form>
-<?php if(file_exists("lists/" . $filename . ".csv")) { ?>
-    <h1><?php echo $filename; ?> Checklist</h1>
+<?php if(file_exists("lists/" . $current . ".csv")) { ?>
+    <h1><?php echo $current; ?> Checklist</h1>
 <?php } else { ?>
     <h1>checkli.st</h1>
     <h2>Flight Simulation Checklist Viewer</h2>
