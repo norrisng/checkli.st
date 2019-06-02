134939ad2324c3d10b81ffef7ff8796db047dc62
Elliot Parker <elliotccp@gmail.com>
2019-06-02 14:53:39 +1000

    Add spacing to monitor/confirm items

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 <tr><td COLSPAN="3"></td></tr>
                 <tr><td COLSPAN="3" style="color:red;"><i><?php echo $line[1]; ?></i></td></tr>
             <?php } else if($line[0] == "-CHECK-") { ?>
-                <tr><td COLSPAN="3" style="text-align:center;"><i><?php echo $line[1]; ?></i></td></tr>
+                <tr><td COLSPAN="3" style="text-align:center;"><br><i><?php echo $line[1]; ?></i><br></td></tr>
             <?php } else { ?>
                 <tr><td COLSPAN="2"><?php echo $line[0]; ?></td><td style="border-left:1px solid black;"><?php echo $line[1]; ?></td></tr>
             <?php } ?>
