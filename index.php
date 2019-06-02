371c58fa4cde364e824b1bf30d1a636b6bcd4839
Elliot Parker <elliotccp@gmail.com>
2019-06-02 14:11:18 +1000

    Update formatting

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <form action="index.php" method="GET">
     <select onchange="this.form.submit()">
         <option>Select Checklist</option>
-    
+    <?php foreach(glob("lists/*.csv") as $filename) {
+        $filename = substr($filename,strpos($filename,"/") + 1, -4); ?>
+        <option value="<?php echo $filename; ?>"><?php echo $filename; ?></option>
+    <?php } ?>
     </select>
 </form>
