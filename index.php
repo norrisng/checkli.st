4341efed782d5153a345e821dff23ba2ba006961
Elliot Parker <elliotccp@gmail.com>
2019-06-02 13:01:20 +1000

    Add dropdown

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+    echo "<select>";
+    foreach(glob("lists/*.csv") as $filename) {
+        echo "<option value=\"" . $filename . "\">" . $filename . "</option>"
+    }
+    echo "</select>";
     if($_GET['l'] == "") {
         echo "<h1>checkli.st</h1>";
         echo "<h2>Flight Simulation Checklist Viewer</h2>";
