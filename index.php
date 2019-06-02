f84032a47c361a9894646d8d1d9ec18545874272
Elliot Parker <elliotccp@gmail.com>
2019-06-02 14:24:15 +1000

    Update dropdown

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form action="/" method="GET">
     <select onchange="this.form.submit()" name="l">
-        <option value="">Select Checklist</option>
+        <option>Select Checklist</option>
     <?php foreach(glob("lists/*.csv") as $filename) {
         $checklist = substr($filename,strpos($filename,"/") + 1, -4); ?>
         <option value="<?php echo $checklist; ?>" <?php if($_GET['l'] == $checklist) {echo "selected";} ?>><?php echo $checklist; ?></option>
