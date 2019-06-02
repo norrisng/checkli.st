e0cb8cc115713ec085bbe2efee2f0a3a11eb4657
Elliot Parker <elliotccp@gmail.com>
2019-06-02 14:10:22 +1000

    Update formatting

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <form action="index.php" method="GET">
     <select onchange="this.form.submit()">
         <option>Select Checklist</option>
-    <?php foreach(glob("lists/*.csv") as $filename) {
-        $filename = substr($filename,strpos($filename,"/") + 1, -4); ?>
-        <option value="<?php echo $filename; ?>"><?php echo $filename; ?></option>
-    <?php } ?>
+    
     </select>
 </form>
-<? if($_GET['l'] == "") { ?>
-    <h1>checkli.st</h1>
-    <h2>Flight Simulation Checklist Viewer</h2>
-<?php } ?>
