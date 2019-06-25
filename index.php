8c6483f85af084ec337e22cec7fed804620438bb
Elliot Parker <elliotccp@gmail.com>
2019-06-25 15:38:40 +1000

    Add links to aircraft list

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php error_reporting(0); ?>
 <?php $current = $_GET['l']; ?>
-<?php 
-    $checklists = array();
-    foreach(glob("lists/*.csv") as $filename) {
-        $file = substr($filename,strpos($filename,"/") + 1, -4);
-        if(strpos($file,"+") !== false) {
-            foreach(explode("+",$file) as $item) {
-                $checklists[$item] = $file;
-            }
-        } else {
-            $checklists[$file] = $file;
-        }
+<?php
+$checklists = array();
+foreach (glob("lists/*.csv") as $filename) {
+  $file = substr($filename, strpos($filename, "/") + 1, -4);
+  if (strpos($file, "+") !== false) {
+    foreach (explode("+", $file) as $item) {
+      $checklists[$item] = $file;
     }
+  } else {
+    $checklists[$file] = $file;
+  }
+}
 ?>
 <html>
 <head>
@@ -24,7 +24,11 @@
     gtag('config', 'UA-119179825-5');
     </script>
 
-    <title><?php if($current == "") {echo "checkli.st | Flight Simulation Checklist Viewer";} else {echo $current . " | checkli.st";} ?></title>
+    <title><?php if ($current == "") {
+      echo "checkli.st | Flight Simulation Checklist Viewer";
+    } else {
+      echo $current . " | checkli.st";
+    } ?></title>
     <meta name="description" content="checkli.st - Flight Simulation Checklist Viewer">
 		
     <meta property="og:site_name" content="checkli.st">
@@ -43,33 +47,49 @@
 <body>
 <form method="GET" autocomplete="off">
     <select class="menu" onchange="if(this.value == '') {window.location.href = 'http://' + window.location.hostname + window.location.pathname;} else {this.form.submit();}" name="l">
-        <option value="" <?php if($current == "") {echo "selected";} ?>>Select Checklist</option>
-    <?php foreach($checklists as $key => $item) { ?>
-        <option value="<?php echo $key; ?>" <?php if($current == $key) {echo "selected";} ?>><?php echo $key; ?></option>
+        <option value="" <?php if ($current == "") {
+          echo "selected";
+        } ?>>Select Checklist</option>
+    <?php foreach ($checklists as $key => $item) { ?>
+        <option value="<?php echo $key; ?>" <?php if ($current == $key) {
+  echo "selected";
+} ?>><?php echo $key; ?></option>
     <?php } ?>
     </select>
 </form>
 <div class="body">
-<?php if(file_exists("lists/" . $checklists[$current] . ".csv")) { ?>
-    <?php $first = true; $checklist = preg_split('/\r\n|\r|\n/', file_get_contents("lists/" . $checklists[$current] . ".csv")); ?>
-        <?php foreach($checklist as $check) { ?>
-            <?php $line = explode(",",$check);
-            if($line[0] == "-GROUP-") {?>
-                <?php if($first == true) { $first = false; ?>
+<?php if (file_exists("lists/" . $checklists[$current] . ".csv")) { ?>
+    <?php
+    $first = true;
+    $checklist = preg_split(
+      '/\r\n|\r|\n/',
+      file_get_contents("lists/" . $checklists[$current] . ".csv")
+    );
+    ?>
+        <?php foreach ($checklist as $check) { ?>
+            <?php
+            $line = explode(",", $check);
+            if ($line[0] == "-GROUP-") { ?>
+                <?php if ($first == true) {
+                  $first = false; ?>
                     <div class="block">
                         <table>
-                <?php } else { ?>
+                <?php
+                } else {
+                   ?>
                         </table>
                     </div>
                     <div class="block">
                         <table>
-                <?php }?>
+                <?php
+                } ?>
                 <span><?php echo $line[1]; ?></span>
-            <?php } else if($line[0] == "-INFO-") { ?>
+            <?php } elseif ($line[0] == "-INFO-") { ?>
                 <tr><td COLSPAN="3" style="text-align:center;"><br><i><?php echo $line[1]; ?></i><br><br></td></tr>
             <?php } else { ?>
                 <tr><td COLSPAN="2"><?php echo $line[0]; ?></td><td class="left"><?php echo $line[1]; ?></td></tr>
-            <?php } ?>
+            <?php }
+            ?>
         <?php } ?>
         </table>
     </div>
@@ -85,8 +105,8 @@
     <br>
     <h3>Current Aircraft Supported:</h3><br>
     <ul>
-    <?php foreach($checklists as $key => $item) { ?>
-        <li><?php echo $key; ?></li>
+    <?php foreach ($checklists as $key => $item) { ?>
+        <li><a href="https://eparker.me/?l=<?php echo $key; ?>"><?php echo $key; ?></a></li>
     <?php } ?>
     </ul>
 <?php } ?>
